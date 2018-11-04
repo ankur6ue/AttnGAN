e48f17d54df9801ab9ffb9d9dc387c7d7e7b9f12
ankur6ue <ankur@umiacs.umd.edu>
2018-11-04 17:11:58 -0600

    first commit

---
+++ client/attnGAN.js
@@ -0,0 +1,146 @@
+/**
+ * Client side of PyTorch Detection Web API
+ * Initial version taken from webrtcHacks - https://webrtchacks.com
+ */
+const s = document.getElementById('attnGAN');
+ const hostType = s.getAttribute("data-apiServer")
+if (hostType == 'localhost')
+	apiServer = "http://127.0.0.1:5000" // must be just like this. using 0.0.0.0 for the IP doesn't work! 
+else
+	apiServer = "https://www.telesens.co/obj_detect_impl"
+
+captions = []
+var activeModel = ""
+
+captions['bird'] = 
+[
+	"this bird is red with white and has a very short beak",
+	"the bird has a yellow crown and a black eyering that is round",
+	"this bird has a green crown black primaries and a white belly",
+	"this bird has wings that are black and has a white belly",
+	"this bird has wings that are red and has a yellow belly",
+	"this bird has wings that are blue and has a red belly",
+	"this is a small light gray bird with a small head and green crown, nape and some green coloring on its wings",
+	"his bird is black with white and has a very short beak",
+	"this small bird has a deep blue crown, back and rump and a bright white belly",
+	"this is a blue bird with a white throat, breast, belly and abdomen and a small black pointed beak",
+	"yellow abdomen with a black eye without an eye ring and beak quite short",
+	"this bird is yellow with black on its head and has a very short beak",
+	"this bird has wings that are black and has a white belly",
+	"this bird has a white belly and breast with a short pointy bill",
+	"this bird is red and white in color with a stubby beak and red eye rings",
+	"a small red and white bird with a small curved beak"
+]
+
+captions['coco'] = 
+[
+	"a photo of a homemade swirly pasta with broccoli carrots and onions",
+	"a fruit stand display with bananas and kiwi",
+	"the girl is surfing a small wave in the water",
+	"a large red and white boat floating on top of a lake",
+	"a herd of cows that are grazing on the grass",
+	"a herd of sheep grazing on a lush green filed",
+	"an old clock next to a light post in front of a steeple",
+	"an image of a girl eating a large slice of pizza",
+	"a clock that is on the side of a tower",
+	"well lit skyscrapers and a clock tower in the evening",
+	"a man kite boarding in the ocean next to a sandy beach",
+	"flat screen television on top of an old tv console"
+]
+
+errorCodes = 
+["bad caption"]
+
+function LoadModel(modelType)
+{
+
+	PopulateExampleCaptions(modelType)
+	
+	let xhr = new XMLHttpRequest();
+	var method = apiServer + '/init/' + modelType
+	xhr.open('GET', method, true);
+	xhr.onload = function () {
+		if (this.status === 200) {
+			$('#status').val(this.response);
+			$('.activeModel').val("Active Model: " + modelType)
+		}
+		else
+		{
+			$('#status').val("Problem Initializing GAN Model");
+		}
+	};
+	xhr.send()
+}
+
+function PopulateExampleCaptions(modelType)
+{
+	var dropdown = $(".dropdown-menu.example-captions");
+	$(".dropdown-menu.example-captions").empty();
+	for( var i = 0; i < captions[modelType].length; i++ )
+	{ 
+		var o = captions[modelType][i];
+		dropdown.append("<li class=\"ExampleCaptionDropDownListItem\" data-name=" + "\"" + o + "\"" + "role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\">" +  o + "</a></li>") 
+	}      
+	$('.ExampleCaptionDropDownListItem').click(function(e) {
+		var name = e.currentTarget;
+		var caption = name.getAttribute("data-name")
+		console.log(caption);
+		$('#caption').val(caption);
+	});
+	
+}
+document.addEventListener("DOMContentLoaded", function() {
+	$('.ModelDropDownListItem').click(function(e) {
+		var name = e.currentTarget;
+		var modelName = name.getAttribute("data-name")
+		console.log(modelName);
+		LoadModel(modelName)
+		activeModel = modelName;
+	});
+	
+	}, false);
+
+function onSubmitCaption()
+{
+	if (!$.trim($("#caption").val())) {// textarea is empty or contains only white-space
+		$('#status').val("Please enter a caption");
+		return;
+	}
+    
+	// Clear status
+	$('#status').val("");
+	
+	let xhr = new XMLHttpRequest();
+	var method = apiServer + '/generate/' + $('#caption').val() + '/' + activeModel;
+    xhr.open('POST', method, true);
+	xhr.onload = function () {
+        if (this.status === 200) {
+			if (errorCodes.includes(this.response)){
+				// There is some error
+				$('#status').val(this.response);
+			}
+			else
+			{
+				var ims = JSON.parse(this.response)
+				var im_256 = ims[0]['im_256']
+				var im_128 = ims[0]['im_128']
+				var im_64 = ims[0]['im_64']
+				var fp_time = ims[0]['fp_time']
+				
+				$('#GeneratedImages_256').html('<img src="data:image/png;base64,' + im_256 + '" class="img-rounded"/>');
+				$('#GeneratedImages_128').html('<img src="data:image/png;base64,' + im_128 + '" class="img-rounded"/>');
+				$('#GeneratedImages_64').html('<img src="data:image/png;base64,' + im_64 + '" class="img-rounded"/>');
+				
+				$('#status').val("Image generated in " + parseFloat(fp_time).toFixed(2) + " seconds");
+			}
+		}
+		if (this.status === 500) { // Internal Server Error
+			// There is some error
+			$('#status').val("Internal Server Error:" + this.response);
+			
+		}
+	};
+	xhr.timeout = 4000;
+	xhr.send(null)
+}
+
---
+++ server/attnGANserver.py
@@ -0,0 +1,280 @@
+from __future__ import print_function
+
+import os
+import sys
+import torch
+import io
+import time
+import numpy as np
+from PIL import Image
+import base64
+import torch.onnx
+from datetime import datetime
+from torch.autograd import Variable
+from miscc.utils import build_super_images2
+from model import RNN_ENCODER, G_NET, set_config
+from miscc.bird_config import bird_cfg
+from miscc.coco_config import coco_cfg
+
+from flask import Flask, request, Response, jsonify, send_file
+
+from threading import Thread
+import logging
+from logging.handlers import RotatingFileHandler
+
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
+
+from werkzeug.contrib.cache import SimpleCache
+cache = SimpleCache()
+
+app = Flask(__name__)
+cfg = bird_cfg
+
+# for CORS
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST')  # Put any other methods you need here
+
+    return response
+
+def vectorize_caption(wordtoix, caption, copies=2):
+    # create caption vector
+    tokens = caption.split(' ')
+    cap_v = []
+    for t in tokens:
+        t = t.strip().encode('ascii', 'ignore').decode('ascii')
+        if len(t) > 0 and t in wordtoix:
+            cap_v.append(wordtoix[t])
+
+    # expected state for single generation
+    captions = np.zeros((copies, len(cap_v)))
+    for i in range(copies):
+        captions[i,:] = np.array(cap_v)
+    cap_lens = np.zeros(copies) + len(cap_v)
+
+    #print(captions.astype(int), cap_lens.astype(int))
+    #captions, cap_lens = np.array([cap_v, cap_v]), np.array([len(cap_v), len(cap_v)])
+    #print(captions, cap_lens)
+    #return captions, cap_lens
+
+    return captions.astype(int), cap_lens.astype(int)
+
+def generate_(caption, modelname, copies=2):
+
+    wordtoix = cache.get(modelname + '_wordtoix')
+    text_encoder = cache.get(modelname + '_text_encoder')
+    netG = cache.get(modelname + '_netG')
+    # load word vector
+    captions, cap_lens  = vectorize_caption(wordtoix, caption, copies)
+    n_words = len(wordtoix)
+    if (cap_lens[0] == 0):
+        return "bad caption"
+    # only one to generate
+    batch_size = captions.shape[0]
+
+    nz = cfg.GAN.Z_DIM
+    captions = Variable(torch.from_numpy(captions), requires_grad = False).type(torch.LongTensor)
+    cap_lens = Variable(torch.from_numpy(cap_lens), requires_grad = False).type(torch.LongTensor)
+    noise = Variable(torch.FloatTensor(batch_size, nz), requires_grad = False)
+
+    if cfg.CUDA:
+        captions = captions.cuda()
+        cap_lens = cap_lens.cuda()
+        noise = noise.cuda()
+
+    #######################################################
+    # (1) Extract text embeddings
+    #######################################################
+    hidden = text_encoder.init_hidden(batch_size)
+    words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
+    mask = (captions == 0)
+        
+
+    #######################################################
+    # (2) Generate fake images
+    #######################################################
+    noise.data.normal_(0, 1)
+    fake_imgs, attention_maps, _, _ = netG(noise, sent_emb, words_embs, mask)
+
+    # ONNX EXPORT
+    #export = os.environ["EXPORT_MODEL"].lower() == 'true'
+    if False:
+        print("saving text_encoder.onnx")
+        text_encoder_out = torch.onnx._export(text_encoder, (captions, cap_lens, hidden), "text_encoder.onnx", export_params=True)
+        print("uploading text_encoder.onnx")
+        print("done")
+
+        print("saving netg.onnx")
+        netg_out = torch.onnx._export(netG, (noise, sent_emb, words_embs, mask), "netg.onnx", export_params=True)
+        print("uploading netg.onnx")
+        print("done")
+        return
+
+    # G attention
+    cap_lens_np = cap_lens.cpu().data.numpy()
+
+
+    # only look at first one
+    #j = 0
+    #for j in range(batch_size):
+    #    for k in range(len(fake_imgs)):
+
+    im = fake_imgs[2][0].data.cpu().numpy()
+    im = (im + 1.0) * 127.5
+    im = im.astype(np.uint8)
+    im = np.transpose(im, (1, 2, 0))
+    im = Image.fromarray(im)
+
+    # save image to stream
+    stream = io.BytesIO()
+    im.save(stream, format="png")
+    im_256 = base64.b64encode(stream.getvalue()).decode()
+
+    im = fake_imgs[1][0].data.cpu().numpy()
+    im = (im + 1.0) * 127.5
+    im = im.astype(np.uint8)
+    im = np.transpose(im, (1, 2, 0))
+    im = Image.fromarray(im)
+
+    # save image to stream
+    stream = io.BytesIO()
+    im.save(stream, format="png")
+    im_128 = base64.b64encode(stream.getvalue()).decode()
+
+    im = fake_imgs[0][0].data.cpu().numpy()
+    im = (im + 1.0) * 127.5
+    im = im.astype(np.uint8)
+    im = np.transpose(im, (1, 2, 0))
+    im = Image.fromarray(im)
+
+    # save image to stream
+    stream = io.BytesIO()
+    im.save(stream, format="png")
+    im_64 = base64.b64encode(stream.getvalue()).decode()
+
+    ret = [{'im_256': im_256, 'im_128': im_128, 'im_64': im_64}]
+    return ret
+
+
+def word_index(model):
+    # model must be 'bird' or 'coco'
+    ixtoword = cache.get(model + '_ixtoword')
+    wordtoix = cache.get(model + '_wordtoix')
+    if ixtoword is None or wordtoix is None:
+        #print("ix and word not cached")
+        # load word to index dictionary
+        x = pickle.load(open('data/' + model + '/captions.pickle', 'rb'))
+        ixtoword = x[2]
+        wordtoix = x[3]
+        del x
+        cache.set(model + '_ixtoword', ixtoword, timeout=60 * 60 * 24)
+        cache.set(model + '_wordtoix', wordtoix, timeout=60 * 60 * 24)
+
+    return wordtoix, ixtoword
+
+def models(modelname, cfg, word_len):
+    #print(word_len)
+    text_encoder = cache.get(modelname + '_text_encoder')
+    if text_encoder is None:
+        #print("text_encoder not cached")
+        text_encoder = RNN_ENCODER(word_len, nhidden=cfg.TEXT.EMBEDDING_DIM)
+        state_dict = torch.load(cfg.TRAIN.NET_E, map_location=lambda storage, loc: storage)
+        text_encoder.load_state_dict(state_dict)
+        if cfg.CUDA:
+            text_encoder.cuda()
+        text_encoder.eval()
+        cache.set(modelname + '_text_encoder', text_encoder, timeout=60 * 60 * 24)
+
+
+    netG = cache.get(modelname + '_netG')
+    if netG is None:
+        #print("netG not cached")
+        netG = G_NET()
+        state_dict = torch.load(cfg.TRAIN.NET_G, map_location=lambda storage, loc: storage)
+        netG.load_state_dict(state_dict)
+        if cfg.CUDA:
+            netG.cuda()
+        netG.eval()
+        cache.set(modelname + '_netG', netG, timeout=60 * 60 * 24)
+
+    return text_encoder, netG
+
+def eval(caption):
+    # load word dictionaries
+    wordtoix, ixtoword = word_index()
+    # lead models
+    text_encoder, netG = models(len(wordtoix))
+
+    t0 = time.time()
+    urls = generate(caption, wordtoix, ixtoword, text_encoder, netG)
+    t1 = time.time()
+
+    response = {
+        'small': urls[0],
+        'medium': urls[1],
+        'large': urls[2],
+        'map1': urls[3],
+        'map2': urls[4],
+        'caption': caption,
+        'elapsed': t1 - t0
+    }
+
+    return response
+
+@app.route('/init/<modelname>')
+def init(modelname):
+    if (os.name == 'posix'):
+        handler = RotatingFileHandler('/home/bitnami/apps/AttnGAN/mesg.log', maxBytes=10000, backupCount=1)
+    if (os.name == 'nt'):
+        handler = RotatingFileHandler('C:\Telesens\web\AttnGAN\client\mesg.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    try:
+        if modelname == 'bird':
+            cfg = bird_cfg
+            set_config(cfg)
+        if modelname == 'coco':
+            cfg = coco_cfg
+            set_config(cfg)
+        if (os.name == 'posix'):
+            cfg.CUDA = False
+        if (os.name == 'nt'):
+            cfg.CUDA = True
+
+        wordtoix, ixtoword = word_index(modelname)
+        # lead models
+        text_encoder, netG = models(modelname, cfg, len(wordtoix))
+        return Response('successfully initialized AttnGAN')
+    except Exception as e:
+        app.logger.info('AttnGAN initialization error: %e' % e)
+        return Response(e)
+
+
+@app.route('/generate/<caption>/<modelname>', methods=['POST'])
+def generate(caption, modelname):
+    try:
+        #caption = "the bird has a yellow crown and a black eyering that is round"
+        t0 = time.time()
+        stream = generate_(caption, modelname)
+        t1 = time.time()
+        print(t1 - t0)
+        stream[0]['fp_time'] = t1-t0
+        return jsonify(stream)
+    except Exception as e:
+        app.logger.info('Error while generating image: %e' % e)
+        print(e)
+        return Response(e)
+
+if __name__ == "__main__":
+    if (os.name == 'nt'):
+        # without SSL
+        app.run(debug=True, host='0.0.0.0', port=5000)
+    #caption = "the bird has a yellow crown and a black eyering that is round"
+
+
